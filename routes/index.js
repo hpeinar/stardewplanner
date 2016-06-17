db3329cf773cd6eff3183c9cd6a98693ea0daeac
Henrik Peinar <henrikpeinar@gmail.com>
2016-06-17 18:00:28 +0300

    Typo fix

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ let RateLimit = require('express-rate-limit');
 
 let limiter = new RateLimit({
     windowMs: 15*60*1000, // 15 minutes
-    max: 600s, // limit each IP to 100 requests per windowMs
+    max: 600, // limit each IP to 100 requests per windowMs
     delayMs: 0 // disable delaying - full speed until the max limit is reached,
 });
 
