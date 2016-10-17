7e7719f2a1876e7ebbd1d46c29422b2fad641720
Henrik Peinar <henrikpeinar@gmail.com>
2016-10-17 23:46:41 +0300

    Electron stuff

--- electron/index.js
+++ electron/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, dialog} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+let closeAgreed = false;
 
 function createWindow () {
     // Create the browser window.
@@ -15,10 +16,35 @@ function createWindow () {
     });
 
     // and load the index.html of the app.
-    win.loadURL(`file://${__dirname}/../public/planner/app.html`);
+    if (process.env.NODE_ENV == 'development') {
+        // in dev the files are in another location
+        win.loadURL(`file://${__dirname}/../public/planner/app.html`);
+    } else {
+        win.loadURL(`file://${__dirname}/public/planner/app.html`);
+    }
 
     // Open the DevTools.
     win.webContents.openDevTools();
+
+
+    win.on('close', function(e) { //   <---- Catch close event
+        console.log('closing');
+        // The dialog box below will open, instead of your app closing.
+        if (!closeAgreed) {
+            e.preventDefault();
+
+            dialog.showMessageBox({
+                message: 'Are you sure you want to quit? You\'ll lose all unsaved progress',
+                buttons: ['Quit', 'Don\'t quit']
+            }, function (button) {
+                console.log('Button pressed', button);
+                if (button === 0) {
+                    closeAgreed = true;
+                    win.close();
+                }
+            });
+        }
+    });
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -39,7 +65,7 @@ app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 });
 
