9db83a1fd5e6040b7a04cd569e693930ddd9eae8
Henrik Peinar <henrikpeinar@gmail.com>
2017-01-09 10:33:08 +0200

    Mysql implementation

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const bunyan = require('bunyan');
 const log = bunyan.createLogger({name: "stardrew"});
 const bodyParser = require('body-parser');
 const app = express();
-const db = require('./lib/db');
 const favicon = require('serve-favicon');
 
 app.enable('trust proxy');
@@ -22,11 +21,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(db.createConnection);
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // currently nothing is at root, redirect use directly to planner
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.redirect('/planner');
 });
 
@@ -34,20 +32,16 @@ app.get('/', function (req, res) {
 app.use('/api', require('./routes')());
 
 // heartbeat endpoint for pinging applications
-app.get('/heartbeat', function (req, res) {
+app.get('/heartbeat', (req, res) => {
     res.sendStatus(200);
 });
-
-app.use(db.closeConnection);
 
 // static mounts
 app.use(express.static('./public'));
 app.use('/planner/:id', express.static('./public/planner'));
 
 
-// do rethinkDB setup and then run express
-db.setup(log).then(function () {
-    app.listen(config.port, function () {
-        log.info('App listening on port', config.port);
-    });
+// run express
+app.listen(config.port, () => {
+    log.info('App listening on port', config.port);
 });
